ef1a5bdd18a3363fc0913ef3277e7da6fc481938
madhukarGit <hmk02330@gmail.com>
2022-12-13 20:21:11 +0530

    comments

--- src/main/java/com/java19/challenge/CountWords.java
+++ src/main/java/com/java19/challenge/CountWords.java
@@ -4,8 +4,12 @@ import java.util.regex.Pattern;
 
 public class CountWords {
     public static void main(String[] args) {
-        var text = "    hey hi  123  hello help   cou-ntry loki_hulk";
-        var regex = "[\\s+]?[\\w-_]+[\\s+]?\\b";
+        /*
+        * count words in text, words can contain - and _ but not digits.
+        * */
+
+        var text = "    hey hi 123 hello help   cou-ntry loki_hulk";
+        var regex = "[\\s+]?[A-Za-z-_]+[\\s+]?\\b";
         var results = Pattern.compile(regex).matcher(text).results().count();
         System.out.println(results);
     }
